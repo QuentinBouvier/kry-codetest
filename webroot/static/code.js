f16c2a2383ec5e253829fa75c280b849a3d548e4
Quentin B <32564163+QuentinBouvier@users.noreply.github.com>
2021-07-16 04:25:29 +0200

    feat: Little improvement on frontend
    * Display the whole info about the services
    * Working form with name and url
    * Simple error display

--- webroot/static/code.js
+++ webroot/static/code.js
@@ -1,24 +1,37 @@
 const listContainer = document.querySelector('#service-list');
 let servicesRequest = new Request('/service');
 fetch(servicesRequest)
-.then(function(response) { return response.json(); })
-.then(function(serviceList) {
-  serviceList.forEach(service => {
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(service.name + ': ' + service.status));
-    listContainer.appendChild(li);
-  });
-});
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (serviceList) {
+        serviceList.forEach(service => {
+            var li = document.createElement("li");
+            li.innerHTML = `<strong>${service.status.padEnd(7, ' ')}</strong> - `;
+            li.appendChild(document.createTextNode(`${service.name} (${service.url})`));
+            listContainer.appendChild(li);
+        });
+    });
 
 const saveButton = document.querySelector('#post-service');
-saveButton.onclick = evt => {
+saveButton.onclick = () => {
+    document.querySelector('#error-container').innerHTML = '';
     let urlName = document.querySelector('#url-name').value;
+    let serviceName = document.querySelector('#service-name').value;
     fetch('/service', {
-    method: 'post',
-    headers: {
-    'Accept': 'application/json, text/plain, */*',
-    'Content-Type': 'application/json'
-    },
-  body: JSON.stringify({url:urlName})
-}).then(res=> location.reload());
+        method: 'post',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({url: urlName, name: serviceName})
+    }).then(res => {
+        if (res.ok) {
+            location.reload()
+        } else {
+            res.text().then(body => {
+                document.querySelector('#error-container').innerHTML = body;
+            })
+        }
+    });
 }
